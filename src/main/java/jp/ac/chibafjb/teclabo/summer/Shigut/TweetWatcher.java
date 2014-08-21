1936be35fa2751b21c4f0b2a93dcda7155566f07
shigure <shigure@git.com>
2014-08-21 15:08:16 +0900

    ハッシュタグ #ff14の監視が可能になりました。

--- src/main/java/jp/ac/chibafjb/teclabo/summer/Shigut/TweetWatcher.java
+++ src/main/java/jp/ac/chibafjb/teclabo/summer/Shigut/TweetWatcher.java
@@ -1,42 +1,79 @@
 package jp.ac.chibafjb.teclabo.summer.Shigut;
 
-import twitter4j.Query;
-import twitter4j.QueryResult;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
+import twitter4j.FilterQuery;
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
 import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
 
 public class TweetWatcher {
 	public void Watcher() {
-		try {
-			// 認証キーを設定
-			ConfigurationBuilder builder = new ConfigurationBuilder();
-			builder.setOAuthConsumerKey("b85u4uPoSjmQGxpw8ecz0hkNV");
-			builder.setOAuthConsumerSecret("sqSnRgTHrust2ApC34my4xCaoXKFsXnwlA9LDaAipLCQjROQYW");
-			builder.setOAuthAccessToken("2751215797-OurAxPurG3DD50LoiNj9gXHfsAu0RB3l1jHyNCq");
-			builder.setOAuthAccessTokenSecret("A0QdIpDP79N52OthL6pMbJW52zfdkdW1bv1gTRDd9FX2U");
-			Configuration conf = builder.build();
-			System.out.println("認証完了");
 
-			// 初期化
-			Twitter twitter = new TwitterFactory(conf).getInstance();
-			Query query = new Query();
-			System.out.println("初期化完了");
-			// 検索条件
-			query.setQuery("バルス");
-			query.setCount(1);
-			System.out.println("条件完了");
+		// 認証キーを設定
+		ConfigurationBuilder builder = new ConfigurationBuilder();
+		builder.setOAuthConsumerKey("b85u4uPoSjmQGxpw8ecz0hkNV");
+		builder.setOAuthConsumerSecret("sqSnRgTHrust2ApC34my4xCaoXKFsXnwlA9LDaAipLCQjROQYW");
+		builder.setOAuthAccessToken("2751215797-OurAxPurG3DD50LoiNj9gXHfsAu0RB3l1jHyNCq");
+		builder.setOAuthAccessTokenSecret("A0QdIpDP79N52OthL6pMbJW52zfdkdW1bv1gTRDd9FX2U");
+		Configuration conf = builder.build();
+		System.out.println("認証完了");
 
-			QueryResult result = twitter.search(query);
+		TwitterStream stream = new TwitterStreamFactory(conf).getInstance();
+		System.out.println("!!!!!");
 
-			System.out.println("ヒット数" + result.getTweets().size());
+		// /////////////////
 
-		} catch (TwitterException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		StatusListener statusListener = new StatusListener() {
+
+			@Override
+			public void onException(Exception arg0) {
+				// TODO 自動生成されたメソッド・スタブ
+
+			}
+
+			@Override
+			public void onTrackLimitationNotice(int arg0) {
+				// TODO 自動生成されたメソッド・スタブ
+
+			}
+
+			@Override
+			public void onStatus(Status status) {
+				System.out.println(status.getText());
+			}
+
+			@Override
+			public void onStallWarning(StallWarning arg0) {
+				// TODO 自動生成されたメソッド・スタブ
+
+			}
+
+
+			@Override
+			public void onDeletionNotice(StatusDeletionNotice arg0) {
+				// TODO 自動生成されたメソッド・スタブ
+
+			}
+
+			@Override
+			public void onScrubGeo(long arg0, long arg1) {
+				// TODO 自動生成されたメソッド・スタブ
+
+			}
+		};
+
+		// ///////////////
+
+		stream.addListener(statusListener);
+
+		FilterQuery filterQuery = new FilterQuery();
+
+		filterQuery.track(new String[] {"#ff14"});
+		stream.filter(filterQuery);
 	}
 
 	public static void main(String[] args) {
